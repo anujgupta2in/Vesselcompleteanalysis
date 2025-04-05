a2f87be82866f67507b06ab42f833166e7fd20c6
anujgupta2in <95157005+anujgupta2in@users.noreply.github.com>
2025-04-05 16:34:17 +0530

    Update engine_processor.py

--- engine_processor.py
+++ engine_processor.py
@@ -290,6 +290,23 @@ def process_engine_data(data, ref_sheet_path=None, engine_type=None):
         cylinder_pivot_table.columns.name = None
 
 
+                # Apply color coding using pandas Styler
+        def highlight_cylinder_cells(val):
+            try:
+                if val == 1:
+                    return 'background-color: green; color: white'
+                elif val == 0:
+                    return 'background-color: red; color: white'
+            except:
+                pass
+            return ''
+        
+        styled_cylinder_pivot_table = cylinder_pivot_table.style.applymap(
+            highlight_cylinder_cells,
+            subset=cylinder_pivot_table.columns[1:]  # Skip 'Cylinder Unit'
+        )
+
+
 
 
 
